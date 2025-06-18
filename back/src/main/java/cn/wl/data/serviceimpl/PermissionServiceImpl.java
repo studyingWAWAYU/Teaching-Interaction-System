36d137211652be7b3c8d6eb5d53dfd68deab5c11
yvette <1104192370@qq.com>
2025-06-18 21:21:50 +0800

    修改菜单权限管理相关代码，适配前端静态菜单，运行无报错

---
+++ back/src/main/java/cn/wl/data/serviceimpl/PermissionServiceImpl.java
@@ -0,0 +1,29 @@
+
+package cn.wl.data.serviceimpl;
+
+import cn.wl.data.entity.Permission;
+import cn.wl.data.dao.mapper.PermissionMapper;
+import cn.wl.data.service.IPermissionService;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class PermissionServiceImpl extends ServiceImpl<PermissionMapper, Permission> implements IPermissionService {
+
+    @Override
+    public Set<String> getPermTypesByUserId(String userId) {
+        List<Permission> permissions = baseMapper.getUserPermissions(userId);
+        return permissions.stream()
+                .filter(p -> p.getPermTypes() != null)
+                .flatMap(p -> p.getPermTypes().stream())
+                .map(String::trim)
+                .collect(Collectors.toSet());
+
+    }
+}
